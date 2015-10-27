e04aeb242b7804ab3b756b5ee89b067248540e5c
xmaoman <marcelo.martin@inatel.br>
2015-10-27 16:53:46 -0200

    Set users permissions

--- common/models/user.js
+++ common/models/user.js
@@ -1,30 +1,30 @@
-module.exports = function(User) {
+module.exports = function(user) {
   //send verification email after registration
-  user.afterRemote('create', function(context, user) {
-    console.log('> user.afterRemote triggered');
-    var options = {
-      type: 'email',
-      to: user.email,
-      from: 'deliveries@inatel.br',
-      subject: 'Deliveries registration confirmation.',
-      template: path.resolve(__dirname, '../../server/views/verify.ejs'),
-      redirect: '/verified',
-      user: user
-    };
-    user.verify(options, function(err, response) {
-      if (err) {
-        next(err);
-        return;
-      }
-      console.log('> verification email sent:', response);
-      context.res.render('response', {
-        title: 'The user has signed up successfully on delivery system',
-        content: 'Please check your email and click on the verification link '
-          + 'to confirm youur email.',
-        redirectTo: '/',
-        redirectToLinkText: 'Log in'
-      });
-    });
-  });
+  // user.afterRemote('create', function(context, user) {
+  //   console.log('> user.afterRemote triggered');
+  //   var options = {
+  //     type: 'email',
+  //     to: user.email,
+  //     from: 'deliveries@inatel.br',
+  //     subject: 'Deliveries registration confirmation.',
+  //     template: path.resolve(__dirname, '../../server/views/verify.ejs'),
+  //     redirect: '/verified',
+  //     user: user
+  //   };
+  //   user.verify(options, function(err, response) {
+  //     if (err) {
+  //       next(err);
+  //       return;
+  //     }
+  //     console.log('> verification email sent:', response);
+  //     context.res.render('response', {
+  //       title: 'The user has signed up successfully on delivery system',
+  //       content: 'Please check your email and click on the verification link '
+  //         + 'to confirm your email.',
+  //       redirectTo: '/',
+  //       redirectToLinkText: 'Log in'
+  //     });
+  //   });
+  // });
 
 };
