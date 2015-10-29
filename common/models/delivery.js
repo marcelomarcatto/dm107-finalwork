d95634a11b75ddcfb8d7946de0992ad16b837c35
marcelomarcatto <marcelo.marcatto@gmail.com>
2015-10-29 00:01:17 -0200

    ListBy date response

--- common/models/delivery.js
+++ common/models/delivery.js
@@ -2,18 +2,37 @@ module.exports = function(Delivery) {
   // Disables auto defined ChangeStreans methods.
   Delivery.disableRemoteMethod('createChangeStream', true);
   Delivery.listDeliveriesByDate = function(cb) {
-      cb(null, 'Test');
+      Delivery.find({
+        where: {
+          deliveryDate: {lt: Date.now()}
+        }
+      }, function(error, results) {
+        var response = {};
+        results.forEach(function(delivery){
+          var date = delivery.deliveryDate;
+
+          if (!response[date]){
+            response[date] = {
+              'deliveriesCount': 0,
+              'isOwnerCount': 0
+            };
+          }
+
+          response[date].deliveriesCount = response[date].deliveriesCount + 1;
+          if (delivery.receiverIsOwner) {
+            response[date].isOwnerCount = response[date].isOwnerCount + 1;
+          }
+        });
+
+        cb(null, response);
+      });
     }
 
     Delivery.remoteMethod(
         'listDeliveriesByDate',
         {
-          //accepts: {arg: 'msg', type: 'string'},
-          returns: [
-            {arg: 'deliveryDate', type: 'date'},
-            {arg: 'deliveriesCount', type: 'int'},
-            {arg: 'isOwnerCount', type: 'int'}
-          ],
+          //accepts: {arg: 'startDate', type: 'date'},
+          returns: {arg: 'response', type: 'object'},
           http: {path: '/listByDate', verb: 'get'}
         }
     );
